7ce102d450d71aec490381a919a278de31aec511
amrinlong <programrin2020@gmail.com>
2023-02-11 20:11:24 -0800

    lesson 29

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,48 @@
 import { useParams } from "react-router-dom"
+import { useQuery } from '@tanstack/react-query';
+import ErrorBoundary from "./ErrorBoundary";
+import Carousel from "./Carousel";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
     const { id } = useParams();
-    return <h2>{id}</h2>;
+    const results = useQuery(["details", id], fetchPet);
+
+    if (results.isError) {
+        return <h2>ohno</h2>
+    }
+
+    if (results.isLoading) {
+        return (
+            <div className="loading-pane">
+                <h2 className="loader">🌀</h2>
+            </div>
+        );
+    }
+
+    const pet = results.data.pets[0];
+
+    return (
+        <div className="details">
+            <Carousel images={pet.images} />
+            <div>
+                <h1>{pet.name}</h1>
+                <h2>
+                    {pet.animal} - {pet.breed} - {pet.city}, {pet.state} 
+                    <button>Adopt {pet.name}</button>
+                    <p>{pet.description}</p>
+                </h2>
+            </div>
+        </div>
+    )
 };
 
-export default Details;
+function DetailsErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <Details {...props} />
+        </ErrorBoundary>
+    )
+}
+
+export default DetailsErrorBoundary;
